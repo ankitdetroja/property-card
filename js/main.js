ea72d04adef8ba759dd05d0a044fbe06ecf02597
ankit detroja <47420910+ankitdetroja@users.noreply.github.com>
2021-09-06 18:30:56 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// slick fade animation
+$('.fade').slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false
+});
